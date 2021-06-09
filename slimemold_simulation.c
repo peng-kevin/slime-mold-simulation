062b8d58e54761ac5a4d80555d6bf489149dcef9
Kevin Peng <kevinpeng02@gmail.com>
2021-06-09 00:20:45 -0700

    Refactored simulation
    Put the parts into their own function.

--- slimemold_simulation.c
+++ slimemold_simulation.c
@@ -28,6 +28,45 @@ float randf(float min, float max) {
     return min + (((float)rand())/RAND_MAX) * (max - min);
 }
 
+void deposit_trail(float *next_map, struct Agent *agent, float trail_deposit_rate, int width) {
+    float updated_trail = fmin(MAX_TRAIL, next_map[(int) agent->y * width + (int) agent->x] + trail_deposit_rate);
+    next_map[(int) agent->y * width + (int) agent->x] = updated_trail;
+}
+
+void check_wall_collision (struct Agent *agent, float *new_x, float *new_y, int width, int height) {
+    if (*new_x < 0) {
+        *new_x = 0;
+        // scatter off of left wall
+        agent->direction = randf(-M_PI_2 + SCATTER_BUFFER, M_PI_2 - SCATTER_BUFFER);
+    } else if (*new_x >= width) {
+        // a little is subtracted from width because x is rounded down and
+        // map[y][width] would be out of bound
+        *new_x = width - EPSILON;
+        // scatter off of right wall
+        agent->direction = randf(M_PI_2 + SCATTER_BUFFER, 3 * M_PI_2 - SCATTER_BUFFER);
+    }
+    // note that the directions are a little weird since y = 0 is the top wall
+    if (*new_y < 0) {
+        *new_y = 0;
+        // scatter off of top wall
+        agent->direction = randf(SCATTER_BUFFER, M_PI - SCATTER_BUFFER);
+    } else if (*new_y >= height) {
+        *new_y = height - EPSILON;
+        // scatter off of bottom wall
+        agent->direction = randf(M_PI + SCATTER_BUFFER, 2 * M_PI - SCATTER_BUFFER);
+    }
+}
+
+void move_and_check_wall_collision (struct Agent *agent, float movement_speed, int width, int height) {
+    // move in direction
+    float new_x = next_x(agent->x, movement_speed, agent->direction);
+    float new_y = next_y(agent->y, movement_speed, agent->direction);
+    // check for collision
+    check_wall_collision(agent, &new_x, &new_y, width, height);
+    // update position
+    agent->x = new_x;
+    agent->y = new_y;
+}
 void simulate_step(float *map, float *next_map, struct Agent *agents, int width,
                     int height, int nagents, float movement_speed,
                     float trail_deposit_rate, float movement_noise,
@@ -43,9 +82,7 @@ void simulate_step(float *map, float *next_map, struct Agent *agents, int width,
     // iterates through each agent
     for (int i = 0; i < nagents; i++) {
         struct Agent *agent = &agents[i];
-        // deposit trail
-        float updated_trail = fmin(255, next_map[(int) agent->y * width + (int) agent->x] + trail_deposit_rate);
-        next_map[(int) agent->y * width + (int) agent->x] = updated_trail;
+        deposit_trail(next_map, agent, trail_deposit_rate, width);
 
         // turn to follow trail
         // follows the highest intesity in the three directions
@@ -62,33 +99,6 @@ void simulate_step(float *map, float *next_map, struct Agent *agents, int width,
             }
         }*/
 
-        // move in direction
-        float new_x = next_x(agent->x, movement_speed, agent->direction);
-        float new_y = next_y(agent->y, movement_speed, agent->direction);
-        // check for collision
-        if (new_x < 0) {
-            new_x = 0;
-            // scatter off of left wall
-            agent->direction = randf(-M_PI_2 + SCATTER_BUFFER, M_PI_2 - SCATTER_BUFFER);
-        } else if (new_x >= width) {
-            // a little is subtracted from width because x is rounded down and
-            // map[y][width] would be out of bound
-            new_x = width - EPSILON;
-            // scatter off of right wall
-            agent->direction = randf(M_PI_2 + SCATTER_BUFFER, 3 * M_PI_2 - SCATTER_BUFFER);
-        }
-        // note that the directions are a little weird since y = 0 is the top wall
-        if (new_y < 0) {
-            new_y = 0;
-            // scatter off of top wall
-            agent->direction = randf(SCATTER_BUFFER, M_PI - SCATTER_BUFFER);
-        } else if (new_y >= height) {
-            new_y = height - EPSILON;
-            // scatter off of bottom wall
-            agent->direction = randf(M_PI + SCATTER_BUFFER, 2 * M_PI - SCATTER_BUFFER);
-        }
-        // update position
-        agent->x = new_x;
-        agent->y = new_y;
+        move_and_check_wall_collision(agent, movement_speed, width, height);
     }
 }
