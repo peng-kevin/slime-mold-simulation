67d42959fe66dd470d3f91e037a4fb079b710c1b
Kevin Peng <kevinpeng02@gmail.com>
2021-06-08 23:59:32 -0700

    Added scattering off of walls
    When an agent hits a wall, it will scatter in a random direction away from the
    wall. The direction will be at least SCATTER_BUFFER rads away from the wall.

--- slimemold_simulation.c
+++ slimemold_simulation.c
@@ -1,12 +1,14 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "simulation.h"
 
 #define LOOK_AHEAD 1.5
 #define EPSILON 0.001
 #define MAX_TRAIL 255
-
+// 10 degrees
+#define SCATTER_BUFFER M_PI/18
 // get the next x after moving distance units in direction
 float next_x(float x, float distance, float direction) {
     return x + distance * cos(direction);
@@ -19,6 +21,11 @@ float next_y(float y, float distance, float direction) {
 // bounds the float between min and max
 float bound(float n, float min, float max) {
     return fmaxf(min, fminf(max, n));
+}
+
+// gets a random float between min and max, not thread safe
+float randf(float min, float max) {
+    return min + (((float)rand())/RAND_MAX) * (max - min);
 }
 
 void simulate_step(float *map, float *next_map, struct Agent *agents, int width,
@@ -61,15 +68,24 @@ void simulate_step(float *map, float *next_map, struct Agent *agents, int width,
         // check for collision
         if (new_x < 0) {
             new_x = 0;
+            // scatter off of left wall
+            agent->direction = randf(-M_PI_2 + SCATTER_BUFFER, M_PI_2 - SCATTER_BUFFER);
         } else if (new_x >= width) {
             // a little is subtracted from width because x is rounded down and
             // map[y][width] would be out of bound
             new_x = width - EPSILON;
+            // scatter off of right wall
+            agent->direction = randf(M_PI_2 + SCATTER_BUFFER, 3 * M_PI_2 - SCATTER_BUFFER);
         }
+        // note that the directions are a little weird since y = 0 is the top wall
         if (new_y < 0) {
             new_y = 0;
+            // scatter off of top wall
+            agent->direction = randf(SCATTER_BUFFER, M_PI - SCATTER_BUFFER);
         } else if (new_y >= height) {
             new_y = height - EPSILON;
+            // scatter off of bottom wall
+            agent->direction = randf(M_PI + SCATTER_BUFFER, 2 * M_PI - SCATTER_BUFFER);
         }
         // update position
         agent->x = new_x;
